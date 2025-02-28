6a7c08878997cb954621dcd1ec7f74715ba9c023
benKapl <benji1.kaplan@gmail.com>
2025-02-28 12:33:10 +0100

    refactored code added printUser helper function

--- handler_user.go
+++ handler_user.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/benKapl/gator/internal/database"
@@ -17,28 +15,23 @@ func handlerRegister(s *state, cmd command) error {
 	}
 	name := cmd.Args[0]
 
-	user, err := s.db.GetUser(context.Background(), name) // Check if user is alreay registered
-	if err == nil {
-		fmt.Fprintf(os.Stderr, "user %s already exists\n", name)
-		os.Exit(1)
-	}
-
-	userParams := database.CreateUserParams{
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 		Name:      name,
-	}
-
-	user, err = s.db.CreateUser(context.Background(), userParams)
+	})
 	if err != nil {
-		return err
+		return fmt.Errorf("couldn't create user: %w", err)
 	}
 
-	s.cfg.CurrentUserName = user.Name
-	fmt.Println("User was successfully created!")
-	fmt.Printf("%+v\n", user)
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
 
+	fmt.Println("User created successfully :")
+	printUser(user)
 	return nil
 }
 
@@ -48,17 +41,21 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	name := cmd.Args[0]
 
-	user, err := s.db.GetUser(context.Background(), name) // Checks that user exists
-	if err == sql.ErrNoRows {
-		fmt.Fprintf(os.Stderr, "user %s does not exist\n", name)
-		os.Exit(1)
+	_, err := s.db.GetUser(context.Background(), name) // Checks that user exists
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
 	}
 
-	err = s.cfg.SetUser(user.Name)
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully!")
 	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
