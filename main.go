452ccdc7576ee0dddbb99fbfde8af9f5d97a0b41
benKapl <benji1.kaplan@gmail.com>
2025-02-28 13:08:33 +0100

    created users command to list users in terminal

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("login", handlerLogin)
 	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerListUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
